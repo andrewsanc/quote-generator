e30b2ffeb27006e59f78010df7076c169b2d6662
andrew <druu831@gmail.com>
2020-12-10 14:53:15 -0800

    added loader functionality

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter-button');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loader
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote() {
+    loading();
     // Proxy request URL
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -15,17 +31,20 @@ async function getQuote() {
         const data = await response.json();
         // If no author quote set to unknown if response quoteAuthor is empty 
         authorText.innerText = (data.quoteAuthor == '') 
-            ? data.quoteAuthor 
-            : 'Unknown';
+            ? 'Unknown'
+            : data.quoteAuthor;
 
         // Reduce font size for long quotes
         quoteText.innerText = (data.quoteText.length > 120) 
             ? quoteText.classList.add('long-quote') 
             : quoteText.classList.remove('long-quote');
-        console.log(data)
+        quoteText.innerText = data.quoteText;
     } catch (error) {
-        console.log('error: ' + error)
+        console.log('error: ' + error);
+        quoteText.innerText = 'An error occured. Refresh or request new quote'
     }
+
+    complete();
 }
 
 // Tweet quote
@@ -42,4 +61,4 @@ newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
-// getQuote();
+getQuote();
